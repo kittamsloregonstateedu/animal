4c20a298dd49e1c6017f98f026b7351c8df0592e
kittamsloregonstateedu <lancekittams@gmail.com>
2023-04-04 18:37:37 -0700

    changed letter p

--- animal.py
+++ animal.py
@@ -3,5 +3,5 @@
 # Date: 04/04/2023
 # Description: Asks the user for their favorite animal and then prints out that that is their favorite animal
 
-fave_animal = input("please enter your favorite animal: ")
+fave_animal = input("Please enter your favorite animal: ")
 print("Your favorite animal is the " + fave_animal + ".")
