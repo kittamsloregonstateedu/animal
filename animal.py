85973c8bb8e3525b9c40c9e8296c31d1b3312bbc
kittamsloregonstateedu <lancekittams@gmail.com>
2023-04-04 18:32:49 -0700

    first commit

---
+++ animal.py
@@ -0,0 +1,7 @@
+# Author: Lance Kittams
+# GitHub username: kittamsloregonstateedu
+# Date: 04/04/2023
+# Description: Asks the user for their favorite animal and then prints out that that is their favorite animal
+
+fave_animal = input("please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
